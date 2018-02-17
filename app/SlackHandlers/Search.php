43b4c75e37258a6a619a7ca42381a3cae9890df2
Sven Luijten <svenluijten1995@gmail.com>
2018-02-17 12:21:58 +0100

    don't respond to 'help'

--- app/SlackHandlers/Search.php
+++ app/SlackHandlers/Search.php
@@ -11,7 +11,7 @@ class Search extends BaseHandler
 {
     public function canHandle(Request $request): bool
     {
-        return true;
+        return $request->text !== 'help';
     }
 
     public function handle(Request $request): Response
